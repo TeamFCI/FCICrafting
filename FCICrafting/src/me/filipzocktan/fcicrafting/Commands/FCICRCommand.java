7c91ba1d964495134b2bb1a38d0901d883a41d51
FilipZocktan <filipzocktan@gmail.com>
2016-03-12 22:38:19 +0100

    Removed FCICR_in_Dev/Set to full version 1.1 pre 4

--- FCICrafting/src/me/filipzocktan/fcicrafting/Commands/FCICRCommand.java
+++ FCICrafting/src/me/filipzocktan/fcicrafting/Commands/FCICRCommand.java
@@ -1,11 +1,31 @@
 package me.filipzocktan.fcicrafting.Commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import me.filipzocktan.fcicrafting.Items.Schwertgenerator;
 
 public class FCICRCommand implements CommandExecutor {
+
+	final private ItemStack INKSACK = new ItemStack(Material.INK_SACK);
+	final private ItemStack DIASCHWERT = new ItemStack(Material.DIAMOND_SWORD);
+	final private ItemStack GLASSCHEIBE_GRAU = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7);
+	final private ItemStack AIR = new ItemStack(Material.AIR, 1, (short) 7);
+	final private ItemStack SLIME = new ItemStack(Material.SLIME_BALL);
+	final private ItemStack KUGELFISCH = new ItemStack(Material.RAW_FISH, 1, (short) 3);
+	final private ItemStack FERM = new ItemStack(Material.FERMENTED_SPIDER_EYE);
+	@SuppressWarnings("deprecation")
+	final private ItemStack WART = new ItemStack(372);
+	@SuppressWarnings("deprecation")
+	final private ItemStack WITHER = new ItemStack(397, 1, (short) 1);
+	public static Inventory inv = null;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -13,113 +33,245 @@ public class FCICRCommand implements CommandExecutor {
 			Player p = (Player) sender;
 			if (args.length == 1) {
 				if (args[0].equalsIgnoreCase("1")) {
-					p.sendMessage("§a§l *-* Craftingrezept Nr. 1 *-*");
-					p.sendMessage("§f[§FCICrafing§f] Blendendes Schwert Tier 1");
-					p.sendMessage("D = Diamantschwert");
-					p.sendMessage("T = Tintensack");
-					p.sendMessage("S = Schleimball");
-					p.sendMessage("N = Nichts");
-					p.sendMessage("N T N");
-					p.sendMessage("S D S");
-					p.sendMessage("N T N");
-					p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+					inv = Bukkit.getServer().createInventory(null, 27, "Blendendes Schwert Tier 1");
+					ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+					meta.setDisplayName(" ");
+					GLASSCHEIBE_GRAU.setItemMeta(meta);
+					for (int i = 0; i < 27; i++) {
+						inv.setItem(i, GLASSCHEIBE_GRAU);
+					}
+					inv.setItem(2, AIR);
+					inv.setItem(3, INKSACK);
+					inv.setItem(4, AIR);
+					inv.setItem(20, AIR);
+					inv.setItem(21, INKSACK);
+					inv.setItem(22, AIR);
+					inv.setItem(12, DIASCHWERT);
+					inv.setItem(11, SLIME);
+					inv.setItem(13, SLIME);
+					inv.setItem(15, Schwertgenerator.getBlendT1(p));
+					p.openInventory(inv);
 				} else {
 					if (args[0].equalsIgnoreCase("2")) {
-						p.sendMessage("§a§l *-* Craftingrezept Nr. 2 *-*");
-						p.sendMessage("§f[§FCICrafing§f] Blendendes Schwert Tier 2");
-						p.sendMessage("D = Diamantschwert");
-						p.sendMessage("T = Tintensack");
-						p.sendMessage("S = Schleimball");
-						p.sendMessage("T S T");
-						p.sendMessage("S D S");
-						p.sendMessage("T S T");
-						p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+						inv = Bukkit.getServer().createInventory(null, 27, "Blendendes Schwert Tier 2");
+						ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+						meta.setDisplayName(" ");
+						GLASSCHEIBE_GRAU.setItemMeta(meta);
+						for (int i = 0; i < 27; i++) {
+							inv.setItem(i, GLASSCHEIBE_GRAU);
+						}
+						/*
+						 * 02 03 04 11 12 13 15 20 21 22
+						 */
+						inv.setItem(2, INKSACK);
+						inv.setItem(3, SLIME);
+						inv.setItem(4, INKSACK);
+						inv.setItem(20, INKSACK);
+						inv.setItem(21, SLIME);
+						inv.setItem(22, INKSACK);
+						inv.setItem(12, DIASCHWERT);
+						inv.setItem(11, SLIME);
+						inv.setItem(13, SLIME);
+						inv.setItem(15, Schwertgenerator.getBlendT2(p));
+						p.openInventory(inv);
 					} else {
 						if (args[0].equalsIgnoreCase("3")) {
-							p.sendMessage("§a§l *-* Craftingrezept Nr. 3 *-*");
-							p.sendMessage("§f[§FCICrafing§f]  Übelkeitsschwert Tier 1");
-							p.sendMessage("D = Diamantschwert");
-							p.sendMessage("K = Kugelfisch");
-							p.sendMessage("N = Nichts");
-							p.sendMessage("N K N");
-							p.sendMessage("K D K");
-							p.sendMessage("N K N");
-							p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+							inv = Bukkit.getServer().createInventory(null, 27, "Übelkeitsschwert Tier 1");
+							ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+							meta.setDisplayName(" ");
+							GLASSCHEIBE_GRAU.setItemMeta(meta);
+							for (int i = 0; i < 27; i++) {
+								inv.setItem(i, GLASSCHEIBE_GRAU);
+							}
+
+							/*
+							 * 02 03 04 11 12 13 15 20 21 22
+							 */
+							inv.setItem(2, AIR);
+							inv.setItem(3, KUGELFISCH);
+							inv.setItem(4, AIR);
+							inv.setItem(20, AIR);
+							inv.setItem(21, KUGELFISCH);
+							inv.setItem(22, AIR);
+							inv.setItem(12, DIASCHWERT);
+							inv.setItem(11, KUGELFISCH);
+							inv.setItem(13, KUGELFISCH);
+							inv.setItem(15, Schwertgenerator.getUebelT1(p));
+							p.openInventory(inv);
 						} else {
 							if (args[0].equalsIgnoreCase("4")) {
-								p.sendMessage("§a§l *-* Craftingrezept Nr. 4 *-*");
-								p.sendMessage("§f[§FCICrafing§f]  Übelkeitsschwert Tier 2");
-								p.sendMessage("D = Diamantschwert");
-								p.sendMessage("K = Kugelfisch");
-								p.sendMessage("K K K");
-								p.sendMessage("K D K");
-								p.sendMessage("K K K");
-								p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+								inv = Bukkit.getServer().createInventory(null, 27, "Übelkeitsschwert Tier 2");
+								ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+								meta.setDisplayName(" ");
+								GLASSCHEIBE_GRAU.setItemMeta(meta);
+								for (int i = 0; i < 27; i++) {
+									inv.setItem(i, GLASSCHEIBE_GRAU);
+								}
+
+								/*
+								 * 02 03 04 11 12 13 15 20 21 22
+								 */
+								inv.setItem(2, KUGELFISCH);
+								inv.setItem(3, KUGELFISCH);
+								inv.setItem(4, KUGELFISCH);
+								inv.setItem(20, KUGELFISCH);
+								inv.setItem(21, KUGELFISCH);
+								inv.setItem(22, KUGELFISCH);
+								inv.setItem(12, DIASCHWERT);
+								inv.setItem(11, KUGELFISCH);
+								inv.setItem(13, KUGELFISCH);
+								inv.setItem(15, Schwertgenerator.getUebelT2(p));
+								p.openInventory(inv);
 							} else {
 								if (args[0].equalsIgnoreCase("5")) {
-									p.sendMessage("§a§l *-* Craftingrezept Nr. 5 *-*");
-									p.sendMessage("§f[§FCICrafing§f]  Veriftungsschwert Tier 1");
-									p.sendMessage("D = Diamantschwert");
-									p.sendMessage("F = Fermentiertes Spinnenauge");
-									p.sendMessage("N = Nichts");
-									p.sendMessage("N F N");
-									p.sendMessage("F D F");
-									p.sendMessage("N F N");
-									p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+									inv = Bukkit.getServer().createInventory(null, 27, "Vergiftungsschwert Tier 1");
+									ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+									meta.setDisplayName(" ");
+									GLASSCHEIBE_GRAU.setItemMeta(meta);
+									for (int i = 0; i < 27; i++) {
+										inv.setItem(i, GLASSCHEIBE_GRAU);
+									}
+
+									/*
+									 * 02 03 04 11 12 13 15 20 21 22
+									 */
+									inv.setItem(2, AIR);
+									inv.setItem(3, FERM);
+									inv.setItem(4, AIR);
+									inv.setItem(20, AIR);
+									inv.setItem(21, FERM);
+									inv.setItem(22, AIR);
+									inv.setItem(12, DIASCHWERT);
+									inv.setItem(11, FERM);
+									inv.setItem(13, FERM);
+									inv.setItem(15, Schwertgenerator.getGiftT1(p));
+									p.openInventory(inv);
 								} else {
 									if (args[0].equalsIgnoreCase("6")) {
-										p.sendMessage("§a§l *-* Craftingrezept Nr. 6 *-*");
-										p.sendMessage("§f[§FCICrafing§f]  Veriftungsschwert Tier 2");
-										p.sendMessage("D = Diamantschwert");
-										p.sendMessage("F = Fermentiertes Spinnenauge");
-										p.sendMessage("F F F");
-										p.sendMessage("F D F");
-										p.sendMessage("F F F");
-										p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+										inv = Bukkit.getServer().createInventory(null, 27, "Vergiftungsschwert Tier 2");
+										ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+										meta.setDisplayName(" ");
+										GLASSCHEIBE_GRAU.setItemMeta(meta);
+										for (int i = 0; i < 27; i++) {
+											inv.setItem(i, GLASSCHEIBE_GRAU);
+										}
+
+										/*
+										 * 02 03 04 11 12 13 15 20 21 22
+										 */
+										inv.setItem(2, FERM);
+										inv.setItem(3, FERM);
+										inv.setItem(4, FERM);
+										inv.setItem(20, FERM);
+										inv.setItem(21, FERM);
+										inv.setItem(22, FERM);
+										inv.setItem(12, DIASCHWERT);
+										inv.setItem(11, FERM);
+										inv.setItem(13, FERM);
+										inv.setItem(15, Schwertgenerator.getGiftT2(p));
+										p.openInventory(inv);
 									} else {
 										if (args[0].equalsIgnoreCase("7")) {
-											p.sendMessage("§a§l *-* Craftingrezept Nr. 7 *-*");
-											p.sendMessage("§f[§FCICrafing§f]  Verlangsamungsschwert Tier 1");
-											p.sendMessage("D = Diamantschwert");
-											p.sendMessage("F = Fermentiertes Spinnenauge");
-											p.sendMessage("W = Netherwarze");
-											p.sendMessage("N = Nichts");
-											p.sendMessage("N F N");
-											p.sendMessage("W D W");
-											p.sendMessage("N F N");
-											p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+											inv = Bukkit.getServer().createInventory(null, 27,
+													"Verlangsamungsschwert Tier 1");
+											ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+											meta.setDisplayName(" ");
+											GLASSCHEIBE_GRAU.setItemMeta(meta);
+											for (int i = 0; i < 27; i++) {
+												inv.setItem(i, GLASSCHEIBE_GRAU);
+											}
+
+											/*
+											 * 02 03 04 11 12 13 15 20 21 22
+											 */
+											inv.setItem(2, AIR);
+											inv.setItem(3, FERM);
+											inv.setItem(4, AIR);
+											inv.setItem(20, AIR);
+											inv.setItem(21, FERM);
+											inv.setItem(22, AIR);
+											inv.setItem(12, DIASCHWERT);
+											inv.setItem(11, WART);
+											inv.setItem(13, WART);
+											inv.setItem(15, Schwertgenerator.getSlowT1(p));
+											p.openInventory(inv);
 										} else {
 											if (args[0].equalsIgnoreCase("8")) {
-												p.sendMessage("§a§l *-* Craftingrezept Nr. 8 *-*");
-												p.sendMessage("§f[§FCICrafing§f]  Verlangsamungsschwert Tier 2");
-												p.sendMessage("D = Diamantschwert");
-												p.sendMessage("F = Fermentiertes Spinnenauge");
-												p.sendMessage("W = Netherwarze");
-												p.sendMessage("F W F");
-												p.sendMessage("W D W");
-												p.sendMessage("F W F");
-												p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+												inv = Bukkit.getServer().createInventory(null, 27,
+														"Verlangsamungsschwert Tier 2");
+												ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+												meta.setDisplayName(" ");
+												GLASSCHEIBE_GRAU.setItemMeta(meta);
+												for (int i = 0; i < 27; i++) {
+													inv.setItem(i, GLASSCHEIBE_GRAU);
+												}
+
+												/*
+												 * 02 03 04 11 12 13 15 20 21 22
+												 */
+												inv.setItem(2, FERM);
+												inv.setItem(3, WART);
+												inv.setItem(4, FERM);
+												inv.setItem(20, FERM);
+												inv.setItem(21, WART);
+												inv.setItem(22, FERM);
+												inv.setItem(12, DIASCHWERT);
+												inv.setItem(11, WART);
+												inv.setItem(13, WART);
+												inv.setItem(15, Schwertgenerator.getSlowT2(p));
+												p.openInventory(inv);
 											} else {
 												if (args[0].equalsIgnoreCase("9")) {
-													p.sendMessage("§a§l *-* Craftingrezept Nr. 9 *-*");
-													p.sendMessage("§f[§FCICrafing§f]  Witherschwert Tier 1");
-													p.sendMessage("D = Diamantschwert");
-													p.sendMessage("W = Witherkopf");
-													p.sendMessage("N = Nichts");
-													p.sendMessage("N W N");
-													p.sendMessage("W D W");
-													p.sendMessage("N W N");
-													p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+													inv = Bukkit.getServer().createInventory(null, 27,
+															"Witherschwert Tier 1");
+													ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+													meta.setDisplayName(" ");
+													GLASSCHEIBE_GRAU.setItemMeta(meta);
+													for (int i = 0; i < 27; i++) {
+														inv.setItem(i, GLASSCHEIBE_GRAU);
+													}
+
+													/*
+													 * 02 03 04 11 12 13 15 20
+													 * 21 22
+													 */
+													inv.setItem(2, AIR);
+													inv.setItem(3, WITHER);
+													inv.setItem(4, AIR);
+													inv.setItem(20, AIR);
+													inv.setItem(21, WITHER);
+													inv.setItem(22, AIR);
+													inv.setItem(12, DIASCHWERT);
+													inv.setItem(11, WITHER);
+													inv.setItem(13, WITHER);
+													inv.setItem(15, Schwertgenerator.getWitherT1(p));
+													p.openInventory(inv);
 												} else {
 													if (args[0].equalsIgnoreCase("10")) {
-														p.sendMessage("§a§l *-* Craftingrezept Nr. 10 *-*");
-														p.sendMessage("§f[§FCICrafing§f]  Witherschwert Tier 1");
-														p.sendMessage("D = Diamantschwert");
-														p.sendMessage("W = Witherkopf");
-														p.sendMessage("W W W");
-														p.sendMessage("W D W");
-														p.sendMessage("W W W");
-														p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+														inv = Bukkit.getServer().createInventory(null, 27,
+																"Witherschwert Tier 2");
+														ItemMeta meta = GLASSCHEIBE_GRAU.getItemMeta();
+														meta.setDisplayName(" ");
+														GLASSCHEIBE_GRAU.setItemMeta(meta);
+														for (int i = 0; i < 27; i++) {
+															inv.setItem(i, GLASSCHEIBE_GRAU);
+														}
+
+														/*
+														 * 02 03 04 11 12 13 15
+														 * 20 21 22
+														 */
+														inv.setItem(2, WITHER);
+														inv.setItem(3, WITHER);
+														inv.setItem(4, WITHER);
+														inv.setItem(20, WITHER);
+														inv.setItem(21, WITHER);
+														inv.setItem(22, WITHER);
+														inv.setItem(12, DIASCHWERT);
+														inv.setItem(11, WITHER);
+														inv.setItem(13, WITHER);
+														inv.setItem(15, Schwertgenerator.getWitherT2(p));
+														p.openInventory(inv);
 													} else {
 														p.sendMessage("§a§l *-*-*-*-*-*-*-*-*-*-*-*");
 														p.sendMessage(
